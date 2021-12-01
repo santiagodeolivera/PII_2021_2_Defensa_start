2390c71099aca9fffd4ae54d8517b7565d756382
Santiago De Olivera <santiago.deolivera@correo.ucu.edu.uy>
2021-12-01 09:32:04 -0300

    Resolve exercise 2

--- p2/src/Library/PublicationItem.cs
+++ p2/src/Library/PublicationItem.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Text;
+
 namespace Ucu.Poo.Defense
 {
     public class PublicationItem
@@ -13,6 +16,20 @@ namespace Ucu.Poo.Defense
             this.Material = material;
             this.Quantity = quantity;
             this.Price = price;
+        }
+
+        // Añadido por el patrón "Don't talk to strangers".
+        public string AsText()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append(this.Quantity);
+            builder.Append(" de ");
+            builder.Append(this.Material.Name);
+            builder.Append(" a $");
+            builder.Append(this.Price);
+
+            return builder.ToString();
         }
     }
 }
