c0622f5ee9ded52e9d47ab92c5712940d005047b
Santiago De Olivera <santiago.deolivera@correo.ucu.edu.uy>
2021-12-01 09:38:02 -0300

    Resolve exercise 3

--- p3/src/Library/Publication.cs
+++ p3/src/Library/Publication.cs
@@ -2,12 +2,21 @@ using System;
 using System.Text;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Ucu.Poo.Defense
 {
     public class Publication
     {
         public DateTime EndDate { get; set; }
+
+        public int Total
+        {
+            get
+            {
+                return this.items.Select(item => item.Total).Sum();
+            }
+        }
 
         public IReadOnlyCollection<PublicationItem> Items
         {
